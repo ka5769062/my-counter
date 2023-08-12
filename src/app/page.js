802bfc69c69081d30d13646994766dfb5d0bd805
Kamran Ali <ka5769062@gmail.com>
2023-08-12 21:37:26 +0500

    Initial commit

---
+++ src/app/page.js
@@ -0,0 +1,19 @@
+import Image from 'next/image'
+import styles from './page.module.css'
+import Counter from './component/Counter'
+
+export default function Home() {
+  return (
+    <main >
+
+  <Counter/>
+   
+
+
+
+
+
+
+          </main>
+  )
+}
